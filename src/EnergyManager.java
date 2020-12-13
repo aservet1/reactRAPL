a799750ec5cb284982ea8fb0a8a6995c4f02a7c2
alejandro <aservet1@binghamton.edu>
2020-12-13 17:53:32 -0500

    added original jRAPL files. Am in the process of modifying them. Already deleted and edited a bunch of them, more is likely to come

---
+++ src/EnergyManager.java
@@ -0,0 +1,39 @@
+
+package jrapl;
+
+public class EnergyManager
+{
+	private static boolean libraryLoaded = false;
+	private static int energyManagersActive = 0 ;
+
+	private native static void profileInit();
+	private native static void profileDealloc();
+
+	private static void loadLibrary() {
+
+		String nativelib = "/home/alejandro/jRAPL/src/libCPUScaler.so";
+		try {
+			//NativeUtils.loadLibraryFromJar("libCPUScaler.so");
+			System.load(nativelib);
+		} catch (Exception e) {
+			System.err.println("ERROR LOADING LIBRARY " + nativelib);
+			e.printStackTrace();
+			System.exit(1);
+		}
+		libraryLoaded = true;
+	}
+
+	public void init() //get a better name
+	{
+		if (!libraryLoaded) loadLibrary();
+		if (energyManagersActive++ == 0) profileInit();
+		ArchSpec.init(); // there's definitely a better way of doing this
+	}
+
+	public void dealloc()
+	{
+		if (--energyManagersActive == 0) profileDealloc();
+	}
+
+}
+
