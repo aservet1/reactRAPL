c652c48e261f8e08ff5f84b346515324f1070dd1
alejandro <aservet1@binghamton.edu>
2021-01-03 18:51:49 -0500

    no build in script, just run

--- backend/start-up-backend.sh
+++ backend/start-up-backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cmd='sudo mvn clean install && sudo java -cp target/httpRaplServer-1.0.jar httpRaplServer.HttpRAPL'
+cmd='sudo java -cp target/httpRaplServer-1.0.jar httpRaplServer.HttpRAPL'
 echo $cmd
-eval $cmd
+$cmd
