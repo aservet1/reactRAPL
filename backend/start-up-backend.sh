e4ee0e297dcd68a0d2fe51637f690147ad855e7d
alejandro <aservet1@binghamton.edu>
2021-01-03 10:55:14 -0500

    echo command then build then run the server on && confirmation :)

--- backend/start-up-backend.sh
+++ backend/start-up-backend.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo java -cp target/httpRaplServer-1.0.jar httpRaplServer.JavaHTTPServer 
+cmd='mvn clean install && sudo java -cp target/httpRaplServer-1.0.jar httpRaplServer.JavaHTTPServer'
+echo $cmd
+eval $cmd
